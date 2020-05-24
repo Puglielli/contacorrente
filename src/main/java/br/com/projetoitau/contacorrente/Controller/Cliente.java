47000fe1c81e5c63c555090f055f60a1749ae6aa
ppuglielli <puglielli14@gmail.com>
2020-05-23 23:45:29 -0300

    add controller and properties

--- src/main/java/br/com/projetoitau/contacorrente/Controller/Cliente.java
+++ src/main/java/br/com/projetoitau/contacorrente/Controller/Cliente.java
@@ -1,4 +1,16 @@
 package br.com.projetoitau.contacorrente.Controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("cliente")
 public class Cliente {
+
+    @GetMapping("")
+    public Integer getAllClients() {
+        return 1;
+    }
+
 }

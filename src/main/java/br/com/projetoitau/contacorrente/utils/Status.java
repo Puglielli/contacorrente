2a114e096c56e0248b0e0b8853f7f9810501be33
ppuglielli <puglielli14@gmail.com>
2020-05-25 15:40:52 -0300

    feat: update status

--- src/main/java/br/com/projetoitau/contacorrente/utils/Status.java
+++ src/main/java/br/com/projetoitau/contacorrente/utils/Status.java
@@ -1,19 +1,16 @@
 package br.com.projetoitau.contacorrente.utils;
 
-public enum ErrorCode {
+public enum Status {
 
-    CPF_CNPJ_ALREADY_EXISTS(1000, "CPF / CNPJ ja existe"),
-    CPF_CNPJ_CANNOT_BE_NULL_OR_EMPTY(1002, "CPF / CNPJ nao pode ser nulo ou vazio"),
-    CPF_CNPJ_NOT_FOUND(1003, "CPF / CNPJ nao existe"),
-    CPF_CNPJ_INVALID(1004, "CPF / CNPJ invalido"),
-    BAD_REQUEST(1005, "Erro ao executar a acao"),
-    ACCOUNT_NOT_FOUND(1006, "Conta nao existe"),
-    INSUFFICIENT_FUNDS(1007, "Saldo Insuficiente");
+    ACTIVE(1, "ATIVO"),
+    INATIVE(0, "INATIVO"),
+    SUCCESS(1, "SUCESSO"),
+    FAILED(0, "FALHA");
 
     private Integer code;
     private String message;
 
-    ErrorCode(Integer code, String message) {
+    Status(Integer code, String message) {
         this.code = code;
         this.message = message;
     }
